847e9f4b31f19dc3a0dbc3bab142f111375b5a69
James Abbott <j.abbott@dundee.ac.uk>
2023-06-30 14:38:55 +0100

    Add mapq filtering over a range of mapq thresholds

--- host_filter.py
+++ host_filter.py
@@ -5,12 +5,21 @@ import logging
 from host_filter import database, workflow
 from os import makedirs
 from pathlib import Path
+import re
 
 db_obj=database.database()
 wf=workflow.workflow()
 
 conf_dbs = db_obj.dbs.keys()
 
+def validate_range(ctx,param,value):
+	match=re.search(r'^([0-9]+)-([0-9]+):([0-9]+)$',value)
+	if match:
+		return(match.group(1),match.group(2),match.group(3))
+	else:
+		raise click.BadParameter("Range format must be start-end:increment i.e. 10-30:5")
+		
+		
 @click.group()
 def cli():
 	makedirs('logs',exist_ok=True)
@@ -56,8 +65,9 @@ def run():
 	pass
 
 @run.command()
-def mapping():
-	wf.map()
+@click.option('-v','--verbose', is_flag=True,help='Generate verbose output')
+def mapping(verbose):
+	wf.map(verbose)
 
 @run.command()
 @click.option('-m','--mapq', required=True, type=click.IntRange(0,40), help='Mappinq quality threshold', 
@@ -65,6 +75,14 @@ def mapping():
 @click.option('-v','--verbose', is_flag=True,help='Generate verbose output')
 def filter(mapq,verbose):
 	wf.filter(mapq,verbose)
-	
+
+@run.command()
+@click.option('-r','--range', required=True, callback=validate_range,
+	      help='Range of mapping qualities to filter i.e. "10-30:5" will carry out separate filtering steps between MAPQ 10 and 30 with a increment of 5')
+@click.option('-v','--verbose', is_flag=True,help='Generate verbose output')
+def range_filter(range,verbose):
+	start_mapq, end_mapq, increment=range
+	wf.range_filter(int(start_mapq),int(end_mapq), int(increment), verbose)
+
 if __name__ == "__main__":
 	cli()
--- host_filter/workflow.py
+++ host_filter/workflow.py
@@ -5,7 +5,17 @@ class workflow:
 	def __init__(self):
 		pass
 		
-	def map(self):
+	def map(self, verbose):
+
+		"""
+		Carries out BWA mapping of paired reads located in `fastq` directory
+
+		Required arguments:
+			verbose(bool): Generate verbose output
+
+		Returns:
+			None
+		"""
 
 		config_file='config.yaml'
 		snakefile='host_filter/etc/Snakefile'
@@ -23,13 +33,15 @@ class workflow:
 			printreason = True,
 			nodes = 100, 
 			latency_wait = 60, 
+			config = {'mapq': None},
 			configfiles = [config_file],
 			cluster_config = cluster_config,
 			drmaa = drmaa, 
 			keepgoing = True, 
 			jobname = "map_{jobid}",
 			shadow_prefix = '/tmp', 
-			use_conda = True
+			use_conda = True,
+			verbose=verbose
 		)
 
 		if status: 
@@ -37,6 +49,17 @@ class workflow:
 		return 1
 	
 	def filter(self, mapq, verbose):
+
+		"""
+		Discards reads mapping to host genome with a mapping quality greater than specified value
+
+		Required parameters:
+			mapq(str): Mapping quality threshold
+			verbose(bool): Generate verbose output
+
+		Returns:
+			None
+		"""
 
 		config_file='config.yaml'
 		snakefile='host_filter/etc/Snakefile'
@@ -67,3 +90,50 @@ class workflow:
 		if status: 
 			return 0
 		return 1
+	
+	def range_filter(self, start_mapq, end_mapq, increment, verbose):
+
+		"""
+		Discards reads mapping to the host genome using a range of different
+		mapping quality thresholds 
+		
+		Required parameters: 
+			start_mapq(int): start of mapping quality range
+			end_mapq(int): end of mapping quality range
+			increment(int): increment size between filtering runs
+
+		Returns:
+			None
+		"""
+
+		config_file='config.yaml'
+		snakefile='host_filter/etc/Snakefile'
+		cluster_config='cluster_config.json'
+
+		with open(config_file,'r') as in_fh:
+			config=yaml.safe_load(in_fh)
+
+		drmaa=config['drmaa']
+
+		for mapq in range(start_mapq, end_mapq, increment):
+			if verbose:
+				print(f'Filtering MAPQ {mapq}')
+			
+			status = snakemake.snakemake(
+			snakefile,
+				targets = ['mapq_filter'],
+				printshellcmds = False,
+				printreason = True,
+				nodes = 100,
+				latency_wait = 60,
+				config={'mapq':mapq},
+				configfiles = [config_file],
+				cluster_config = cluster_config,
+				drmaa = drmaa,
+				keepgoing = True,
+				jobname = "filter_mapq_{mapq}_{jobid}",
+				shadow_prefix = '/tmp',
+				verbose=verbose
+			)
+			if not status:
+				raise Exception('Snakemake failed filtering MAPQ {mapq}')
